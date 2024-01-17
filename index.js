7df41a72477dac30f38b5c1c78022095f30a48d6
Samran Haider <s4haider@uwaterloo.ca>
2024-01-17 00:54:17 -0500

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.json("api running")
+})
+app.listen('5000', () =>{
+    console.log("Api running on port 5001")
+})
