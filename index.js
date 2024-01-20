5a653a61bdbd8b0a93b1ed77e7765776755d7e90
Samran Haider <58138217+Samran-spec@users.noreply.github.com>
2024-01-19 23:59:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.json("api running CICD freestyle")
+    res.json("api running CICD ")
 })
 app.listen('5000', () =>{
     console.log("Api running on port 5001")
